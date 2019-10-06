85a79fe46973962c0d3666b4b55d35af284461ef
Stijn Bannink <stijnbannink40@gmail.com>
2019-10-06 16:10:21 +0200

    Update CraftmotoPluginListener.java

--- src/me/wouter/vehiclelocker/listeners/CraftmotoPluginListener.java
+++ src/me/wouter/vehiclelocker/listeners/CraftmotoPluginListener.java
@@ -12,23 +12,23 @@ import net.jordan.vehicles.nms.CustomVehicle;
 
 public class CraftmotoPluginListener implements Listener {
 
-	@EventHandler
-	public void onSeatChange(PlayerChangeSeatEvent e) {
-		CustomVehicle vehicle = e.getTo();
-		Player player = e.getPlayer();
-		if (vehicle.getOwnerUUID() != player.getUniqueId()) {
-			if (Bukkit.getPlayer(vehicle.getOwnerUUID()) != null) {
-				Player owner = Bukkit.getPlayer(vehicle.getOwnerUUID());
-				if (DataFile.getInstance().getData().getBoolean(owner.getUniqueId() + ".Locked")) {
-					player.sendMessage(Main.cc("VehicleIsLocked").replaceAll("<Player>", owner.getName()));
-					e.setCancelled(true);
-				}
-			}
-		} else {
-			if (DataFile.getInstance().getData().getBoolean(player.getUniqueId() + ".Locked")) {
-				player.sendMessage(Main.cc("VehicleIsLockedAlert"));
-			}
-		}
-	}
+    @EventHandler
+    public void onSeatChange(PlayerChangeSeatEvent e) {
+        CustomVehicle vehicle = e.getTo();
+        Player player = e.getPlayer();
+        if (vehicle.getOwnerUUID() != player.getUniqueId()) {
+            if (Bukkit.getPlayer(vehicle.getOwnerUUID()) != null) {
+                Player owner = Bukkit.getPlayer(vehicle.getOwnerUUID());
+                if (DataFile.getInstance().getData().getBoolean(owner.getUniqueId() + ".Locked")) {
+                    player.sendMessage(Main.cc("VehicleIsLocked").replaceAll("<Player>", owner.getName()));
+                    e.setCancelled(true);
+                }
+            }
+        } else {
+            if (DataFile.getInstance().getData().getBoolean(player.getUniqueId() + ".Locked")) {
+                player.sendMessage(Main.cc("VehicleIsLockedAlert"));
+            }
+        }
+    }
 
 }
