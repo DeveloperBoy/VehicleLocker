81a2b3c2858b86f6faeba6aef9ea65775bdcef45
Stijn Bannink <stijnbannink40@gmail.com>
2019-10-06 16:09:59 +0200

    Update DataFile.java

--- src/me/wouter/vehiclelocker/data/DataFile.java
+++ src/me/wouter/vehiclelocker/data/DataFile.java
@@ -9,41 +9,41 @@ import org.bukkit.plugin.Plugin;
 
 public class DataFile {
 
-	static DataFile instance = new DataFile();
+    static DataFile instance = new DataFile();
 
-	public static DataFile getInstance() {
-		return instance;
-	}
+    public static DataFile getInstance() {
+        return instance;
+    }
 
-	FileConfiguration PlayerData;
-	File pdfile;
-	Plugin pl;
+    FileConfiguration PlayerData;
+    File pdfile;
+    Plugin pl;
 
-	public void setup(Plugin pl) {
-		pdfile = new File(pl.getDataFolder(), "Data/Spelers.yml");
-		PlayerData = YamlConfiguration.loadConfiguration(pdfile);
-		this.pl = pl;
-	}
+    public void setup(Plugin pl) {
+        pdfile = new File(pl.getDataFolder(), "Data/Spelers.yml");
+        PlayerData = YamlConfiguration.loadConfiguration(pdfile);
+        this.pl = pl;
+    }
 
-	public FileConfiguration getData() {
-		return PlayerData;
-	}
+    public FileConfiguration getData() {
+        return PlayerData;
+    }
 
-	public void reloadData() {
-		PlayerData = YamlConfiguration.loadConfiguration(pdfile);
-	}
+    public void reloadData() {
+        PlayerData = YamlConfiguration.loadConfiguration(pdfile);
+    }
 
-	public void saveData() {
-		try {
-			if (PlayerData != null && pdfile != null) {
-				PlayerData.save(pdfile);
-			} else {
-				setup(pl);
-				PlayerData.save(pdfile);
-			}
-		} catch (Exception e) {
-			pl.getLogger().info(ChatColor.RED + "Het opslaan van Data.yml is niet gelukt!");
-			e.printStackTrace();
-		}
-	}
+    public void saveData() {
+        try {
+            if (PlayerData != null && pdfile != null) {
+                PlayerData.save(pdfile);
+            } else {
+                setup(pl);
+                PlayerData.save(pdfile);
+            }
+        } catch (Exception e) {
+            pl.getLogger().info(ChatColor.RED + "Het opslaan van Data.yml is niet gelukt!");
+            e.printStackTrace();
+        }
+    }
 }
